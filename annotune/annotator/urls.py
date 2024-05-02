4e82abeee947313cfd3568f0a12d87494d16f099
daniel-stephens <danystevo@gmail.com>
2024-05-02 05:40:12 -0400

    load files page, show documents page frontend completed

--- annotune/annotator/urls.py
+++ annotune/annotator/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+urlpatterns = [ 
     path("", views.login, name="login"),
     path("sign-up", views.sign_up, name="sign-up"),
     path("homepage", views.homepage, name="homepage"),
+    path("load-files", views.load_files, name="load-files"),
+    path("documents", views.list_documents, name="documents"),
+    path("label", views.label, name="label"),
 ]
--- annotune/annotator/views.py
+++ annotune/annotator/views.py
@@ -9,3 +9,42 @@ def sign_up(request):
 
 def homepage(request):
     return render(request, "homepage.html")
+
+def load_files(request):
+    return render(request, "load_files.html")
+
+def list_documents(request):
+    return render(request, "documents.html", {"range":range(10)})
+
+def label (request):
+    documenttext = """A major French union warned of possible strikes, including at hospitals, during 
+    the Paris Olympics, when a massive influx of people is in the French capital. Speaking to France 
+    Info media on Thursday, the general secretary of the CGT said the union will give a notice of 
+    strike in public services during the Games, which are held in July-August. The Paralympics take 
+    place in August-September. Paris' tourism office predicts up to 15.9 million people could visit 
+    the Paris region during July-September. “We want the government to take immediate action to ensure 
+    the success of the Games,” Sophie Binet said. “For this to happen, our warnings must be heeded and 
+    the Games must be prepared from a social point of view. We've been saying the same thing for months 
+    now, and no one cares. It's getting very tiresome."""
+
+    
+
+    document_id = 1
+    number = 1
+
+    label= "Labor Disruption Threat during Paris Olympics"
+
+    explanation = """A French union, CGT, warns of possible strikes during the Paris Olympics due to 
+    social concerns. With up to 15.9 million visitors expected, essential services, notably healthcare, 
+    could be affected. The union demands government action for the Games' success, highlighting 
+    potential labor unrest and logistical challenges."""
+
+
+    data = {
+        "document": documenttext,
+        "label":label,
+        "explanation":explanation,
+        "number": number
+        }
+
+    return render(request, "label.html", data)
